8982f6cdc2d791b2fbbe4f45ee6c31cda339b09c
Ryan Gibbons <insanity5902@gmail.com>
2011-03-31 18:10:48 -0500

    move all elements into object literal

--- warning.js
+++ warning.js
@@ -41,152 +41,146 @@
             }
         }
 
-        // oldschool from gcode
-        var _body = document.getElementsByTagName('body')[0];
-        var _d = document.createElement('div');
-        var _l = document.createElement('div');
-        var _h = document.createElement('h1');
-        var _p1 = document.createElement('p');
-        var _p2 = document.createElement('p');
-        var _ul = document.createElement('ul');
-        var _li1 = document.createElement('li');
-        var _li2 = document.createElement('li');
-        var _li3 = document.createElement('li');
-        var _li4 = document.createElement('li');
-        var _li5 = document.createElement('li');
-        var _ico1 = document.createElement('div');
-        var _ico2 = document.createElement('div');
-        var _ico3 = document.createElement('div');
-        var _ico4 = document.createElement('div');
-        var _ico5 = document.createElement('div');
-        var _lit1 = document.createElement('div');
-        var _lit2 = document.createElement('div');
-        var _lit3 = document.createElement('div');
-        var _lit4 = document.createElement('div');
-        var _lit5 = document.createElement('div');
-        _body.appendChild(_l);
-        _body.appendChild(_d);
-        _d.appendChild(_h);
-        _d.appendChild(_p1);
-        _d.appendChild(_p2);
-        _d.appendChild(_ul);
-        _ul.appendChild(_li1);
-        _ul.appendChild(_li2);
-        _ul.appendChild(_li3);
-        _ul.appendChild(_li4);
-        _ul.appendChild(_li5);
-        _li1.appendChild(_ico1);
-        _li2.appendChild(_ico2);
-        _li3.appendChild(_ico3);
-        _li4.appendChild(_ico4);
-        _li5.appendChild(_ico5);
-        _li1.appendChild(_lit1);
-        _li2.appendChild(_lit2);
-        _li3.appendChild(_lit3);
-        _li4.appendChild(_lit4);
-        _li5.appendChild(_lit5);
-        _d.setAttribute('id','_d');
-        _l.setAttribute('id','_l');
-        _h.setAttribute('id','_h');
-        _p1.setAttribute('id','_p1');
-        _p2.setAttribute('id','_p2');
-        _ul.setAttribute('id','_ul');
-        _li1.setAttribute('id','_li1');
-        _li2.setAttribute('id','_li2');
-        _li3.setAttribute('id','_li3');
-        _li4.setAttribute('id','_li4');
-        _li5.setAttribute('id','_li5');
-        _ico1.setAttribute('id','_ico1');
-        _ico2.setAttribute('id','_ico2');
-        _ico3.setAttribute('id','_ico3');
-        _ico4.setAttribute('id','_ico4');
-        _ico5.setAttribute('id','_ico5');
-        _lit1.setAttribute('id','_lit1');
-        _lit2.setAttribute('id','_lit2');
-        _lit3.setAttribute('id','_lit3');
-        _lit4.setAttribute('id','_lit4');
-        _lit5.setAttribute('id','_lit5');
-        _d.style.zIndex = 99999;
+        var _html = {
+            d: document.createElement('div'),
+            l: document.createElement('div'),
+            h: document.createElement('h1'),
+            p1: document.createElement('p'),
+            p2: document.createElement('p'),
+            ul: document.createElement('ul'),
+            li1: document.createElement('li'),
+            li2: document.createElement('li'),
+            li3: document.createElement('li'),
+            li4: document.createElement('li'),
+            li5: document.createElement('li'),
+            ico1: document.createElement('div'),
+            ico2: document.createElement('div'),
+            ico3: document.createElement('div'),
+            ico4: document.createElement('div'),
+            ico5: document.createElement('div'),
+            lit1: document.createElement('div'),
+            lit2: document.createElement('div'),
+            lit3: document.createElement('div'),
+            lit4: document.createElement('div'),
+            lit5: document.createElement('div')
+        }
+
+        document.body.appendChild(_html.l);
+        document.body.appendChild(_html.d);
+
+        _html.d.appendChild(_html.h);
+        _html.d.appendChild(_html.p1);
+        _html.d.appendChild(_html.p2);
+        _html.d.appendChild(_html.ul);
+        _html.ul.appendChild(_html.li1);
+        _html.ul.appendChild(_html.li2);
+        _html.ul.appendChild(_html.li3);
+        _html.ul.appendChild(_html.li4);
+        _html.ul.appendChild(_html.li5);
+        _html.li1.appendChild(_html.ico1);
+        _html.li2.appendChild(_html.ico2);
+        _html.li3.appendChild(_html.ico3);
+        _html.li4.appendChild(_html.ico4);
+        _html.li5.appendChild(_html.ico5);
+        _html.li1.appendChild(_html.lit1);
+        _html.li2.appendChild(_html.lit2);
+        _html.li3.appendChild(_html.lit3);
+        _html.li4.appendChild(_html.lit4);
+        _html.li5.appendChild(_html.lit5);
+        _html.d.setAttribute('id','_d');
+        _html.l.setAttribute('id','_l');
+        _html.h.setAttribute('id','_h');
+        _html.p1.setAttribute('id','_p1');
+        _html.p2.setAttribute('id','_p2');
+        _html.ul.setAttribute('id','_ul');
+        _html.li1.setAttribute('id','_li1');
+        _html.li2.setAttribute('id','_li2');
+        _html.li3.setAttribute('id','_li3');
+        _html.li4.setAttribute('id','_li4');
+        _html.li5.setAttribute('id','_li5');
+        _html.ico1.setAttribute('id','_ico1');
+        _html.ico2.setAttribute('id','_ico2');
+        _html.ico3.setAttribute('id','_ico3');
+        _html.ico4.setAttribute('id','_ico4');
+        _html.ico5.setAttribute('id','_ico5');
+        _html.lit1.setAttribute('id','_lit1');
+        _html.lit2.setAttribute('id','_lit2');
+        _html.lit3.setAttribute('id','_lit3');
+        _html.lit4.setAttribute('id','_lit4');
+        _html.lit5.setAttribute('id','_lit5');
+        _html.d.style.zIndex = 99999;
+
         var _width = document.documentElement.clientWidth;
         var _height = document.documentElement.clientHeight;
-        var _dl = document.getElementById('_l');
-        _dl.style.zIndex = 99998;
-        _dl.style.width =  _width+"px";
-        _dl.style.height = _height+"px";
-        _dl.style.position = "absolute";
-        _dl.style.top = "0px";
-        _dl.style.left = "0px";
-        _dl.style.filter = "alpha(opacity=50)";
-        _dl.style.background = "#fff";
-        var _dd = document.getElementById('_d');
-        _ddw = 650;
-        _ddh = 260;
-        _dd.style.width = _ddw+"px";
-        _dd.style.height = _ddh+"px";
-        _dd.style.position = "absolute";
-        _dd.style.top = ((_height-_ddh)/2)+"px";
-        _dd.style.left = ((_width-_ddw)/2)+"px";
-        _dd.style.padding = "20px";
-        _dd.style.background = "#fff";
-        _dd.style.border = "1px solid #ccc";
-        _dd.style.fontFamily = "'Lucida Grande','Lucida Sans Unicode',Arial,Verdana,sans-serif";
-        _dd.style.listStyleType = "none";
-        _dd.style.color = "#4F4F4F";
-        _dd.style.fontSize = "12px";
-        _h.appendChild(document.createTextNode(l10n.msg1));
-        var _hd = document.getElementById('_h');
-        _hd.style.display = "block";
-        _hd.style.fontSize = "1.3em";
-        _hd.style.marginBottom = "0.5em";
-        _hd.style.color = "#333";
-        _hd.style.fontFamily = "Helvetica,Arial,sans-serif";
-        _hd.style.fontWeight = "bold";
-        _p1.appendChild(document.createTextNode(l10n.msg2));
-        var _p1d = document.getElementById('_p1');
-        _p1d.style.marginBottom = "1em";
-        _p2.appendChild(document.createTextNode(l10n.msg3));
-        var _p2d = document.getElementById('_p2');
-        _p2d.style.marginBottom = "1em";
-        var _uld = document.getElementById('_ul');
-        _uld.style.listStyleImage = "none";
-        _uld.style.listStylePosition = "outside";
-        _uld.style.listStyleType = "none";
-        _uld.style.margin = "0 px auto";
-        _uld.style.padding = "0px";
-        _uld.style.paddingLeft = "10px";
-        var _li1d = document.getElementById('_li1');
-        var _li2d = document.getElementById('_li2');
-        var _li3d = document.getElementById('_li3');
-        var _li4d = document.getElementById('_li4');
-        var _li5d = document.getElementById('_li5');
-        var _li1ds = _li1d.style;
-        var _li2ds = _li2d.style;
-        var _li3ds = _li3d.style;
-        var _li4ds = _li4d.style;
-        var _li5ds = _li5d.style;
+        _html.l.style.zIndex = 99998;
+        _html.l.style.width =  _width+"px";
+        _html.l.style.height = _height+"px";
+        _html.l.style.position = "absolute";
+        _html.l.style.top = "0px";
+        _html.l.style.left = "0px";
+        _html.l.style.filter = "alpha(opacity=50)";
+        _html.l.style.background = "#fff";
+
+        var _ddw = 650;
+        var _ddh = 260;
+        _html.d.style.width = _ddw+"px";
+        _html.d.style.height = _ddh+"px";
+        _html.d.style.position = "absolute";
+        _html.d.style.top = ((_height-_ddh)/2)+"px";
+        _html.d.style.left = ((_width-_ddw)/2)+"px";
+        _html.d.style.padding = "20px";
+        _html.d.style.background = "#fff";
+        _html.d.style.border = "1px solid #ccc";
+        _html.d.style.fontFamily = "'Lucida Grande','Lucida Sans Unicode',Arial,Verdana,sans-serif";
+        _html.d.style.listStyleType = "none";
+        _html.d.style.color = "#4F4F4F";
+        _html.d.style.fontSize = "12px";
+        _html.h.appendChild(document.createTextNode(l10n.msg1));
+
+        _html.h.style.display = "block";
+        _html.h.style.fontSize = "1.3em";
+        _html.h.style.marginBottom = "0.5em";
+        _html.h.style.color = "#333";
+        _html.h.style.fontFamily = "Helvetica,Arial,sans-serif";
+        _html.h.style.fontWeight = "bold";
+        _html.p1.appendChild(document.createTextNode(l10n.msg2));
+
+        _html.p1.style.marginBottom = "1em";
+        _html.p2.appendChild(document.createTextNode(l10n.msg3));
+
+        _html.p2.style.marginBottom = "1em";
+
+        _html.ul.style.listStyleImage = "none";
+        _html.ul.style.listStylePosition = "outside";
+        _html.ul.style.listStyleType = "none";
+        _html.ul.style.margin = "0 px auto";
+        _html.ul.style.padding = "0px";
+        _html.ul.style.paddingLeft = "10px";
+
+        var _li1ds = _html.li1.style;
+        var _li2ds = _html.li2.style;
+        var _li3ds = _html.li3.style;
+        var _li4ds = _html.li4.style;
+        var _li5ds = _html.li5.style;
         _li1ds.background = _li2ds.background = _li3ds.background = _li4ds.background = _li5ds.background = imagesStyle.background.bg;
         _li1ds.width = _li2ds.width = _li3ds.width = _li4ds.width = _li5ds.width = imagesStyle.background.width;
         _li1ds.height = _li2ds.height = _li3ds.height = _li4ds.height = _li5ds.height = imagesStyle.background.height;
         _li1ds.cursor = _li2ds.cursor = _li3ds.cursor = _li4ds.cursor = _li5ds.cursor = "pointer";
-        _li1d.onclick = function() {window.location = l10n.url1; };
-        _li2d.onclick = function() {window.location = l10n.url2; };
-        _li3d.onclick = function() {window.location = l10n.url3; };
-        _li4d.onclick = function() {window.location = l10n.url4; };
-        _li5d.onclick = function() {window.location = l10n.url5; };
+        _html.li1.onclick = function() {window.location = l10n.url1; };
+        _html.li2.onclick = function() {window.location = l10n.url2; };
+        _html.li3.onclick = function() {window.location = l10n.url3; };
+        _html.li4.onclick = function() {window.location = l10n.url4; };
+        _html.li5.onclick = function() {window.location = l10n.url5; };
         _li1ds.styleFloat = _li2ds.styleFloat = _li3ds.styleFloat = _li4ds.styleFloat = _li5ds.styleFloat = "left";
         //_li1ds.width = _li2ds.width = _li3ds.width = _li4ds.width = _li5ds.width = "120px";
         //_li1ds.height = _li2ds.height = _li3ds.height = _li4ds.height = _li5ds.height = "122px";
         _li1ds.margin = _li2ds.margin = _li3ds.margin = _li4ds.margin = _li5ds.margin = "0 10px 10px 0";
-        var _ico1d = document.getElementById('_ico1');
-        var _ico2d = document.getElementById('_ico2');
-        var _ico3d = document.getElementById('_ico3');
-        var _ico4d = document.getElementById('_ico4');
-        var _ico5d = document.getElementById('_ico5');
-        var _ico1ds = _ico1d.style;
-        var _ico2ds = _ico2d.style;
-        var _ico3ds = _ico3d.style;
-        var _ico4ds = _ico4d.style;
-        var _ico5ds = _ico5d.style;
+
+        var _ico1ds = _html.ico1.style;
+        var _ico2ds = _html.ico2.style;
+        var _ico3ds = _html.ico3.style;
+        var _ico4ds = _html.ico4.style;
+        var _ico5ds = _html.ico5.style;
         _ico1ds.width = _ico2ds.width = _ico3ds.width = _ico4ds.width = _ico5ds.width = "100px";
         _ico1ds.height = _ico2ds.height = _ico3ds.height = _ico4ds.height = _ico5ds.height = "100px";
         _ico1ds.margin = _ico2ds.margin = _ico3ds.margin = _ico4ds.margin = _ico5ds.margin = "1px auto";
@@ -195,21 +189,17 @@
         _ico3ds.background = imagesStyle.safari.bg;
         _ico4ds.background = imagesStyle.opera.bg 
         _ico5ds.background = imagesStyle.chrome.bg;
-        _lit1.appendChild(document.createTextNode(l10n.br1));
-        _lit2.appendChild(document.createTextNode(l10n.br2));
-        _lit3.appendChild(document.createTextNode(l10n.br3));
-        _lit4.appendChild(document.createTextNode(l10n.br4));
-        _lit5.appendChild(document.createTextNode(l10n.br5));
-        var _lit1d = document.getElementById('_lit1');
-        var _lit2d = document.getElementById('_lit2');
-        var _lit3d = document.getElementById('_lit3');
-        var _lit4d = document.getElementById('_lit4');
-        var _lit5d = document.getElementById('_lit5');
-        var _lit1ds = _lit1d.style;
-        var _lit2ds = _lit2d.style;
-        var _lit3ds = _lit3d.style;
-        var _lit4ds = _lit4d.style;
-        var _lit5ds = _lit5d.style;
+        _html.lit1.appendChild(document.createTextNode(l10n.br1));
+        _html.lit2.appendChild(document.createTextNode(l10n.br2));
+        _html.lit3.appendChild(document.createTextNode(l10n.br3));
+        _html.lit4.appendChild(document.createTextNode(l10n.br4));
+        _html.lit5.appendChild(document.createTextNode(l10n.br5));
+
+        var _lit1ds = _html.lit1.style;
+        var _lit2ds = _html.lit2.style;
+        var _lit3ds = _html.lit3.style;
+        var _lit4ds = _html.lit4.style;
+        var _lit5ds = _html.lit5.style;
         _lit1ds.color = _lit2ds.color = _lit3ds.color = _lit4ds.color = _lit5ds.color = "#808080";
         _lit1ds.fontSize = _lit2ds.fontSize = _lit3ds.fontSize = _lit4ds.fontSize = _lit5ds.fontSize = "0.8em";
         _lit1ds.height = _lit2ds.height = _lit3ds.height = _lit4ds.height = _lit5ds.height = "18px";
