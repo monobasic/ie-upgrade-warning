ac637adb7ec7dc62fdfdde68f615b35f212bb752
André Abt <info@monobasic.com>
2012-07-17 09:48:17 +0200

    added german localization

---
+++ localization/de_DE.js
@@ -0,0 +1,21 @@
+//render this via backend
+window.IE6WarningLocalizations ={
+    msg1 : "Wissen Sie, dass Ihr Internet Explorer Browser nicht mehr aktuell ist?",
+    msg2 : "Um unsere Webseite zu nutzen, empfehlen wir Ihnen Ihren Browser auf eine aktuellere Version zu aktualisieren oder einen anderen Webbrowser zu nutzen. Eine Liste der populärsten Browser finden Sie weiter unten.",
+    msg3 : "Klicken Sie auf eines der Symbole um auf die Download-Seite zu gelangen",
+    br: {
+        ie: "Internet Explorer 9+",
+        ff: "Firefox 4+",
+        safari: "Safari 5+",
+        opera: "Opera 11+",
+        chrome: "Chrome 10.0+"
+    },
+    url: {
+        ie: "http://www.microsoft.com/windows/Internet-explorer/default.aspx",
+        ff: "http://www.mozilla.com/firefox/",
+        safari: "http://www.apple.com/safari/download/",
+        opera: "http://www.opera.com/download/",
+        chrome: "http://www.google.com/chrome"
+    }
+};
+
